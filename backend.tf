589ef3f32e8dec4e2bba6a4409ebb2121272bf11
MarianaHru <mariana.hrudzinska.ri.2024@lpnu.ua>
2026-03-03 15:01:55 +0000

    Add api gateway

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "it-step-lab-terraform-state-grudzinska"
-    key            = "dev/domain1/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
